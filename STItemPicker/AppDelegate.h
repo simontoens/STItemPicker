d2ff904b1d12381cbe9e8b39d450db1503c98239
Simon Toens <simontoens@gmail.com>
2013-03-24 19:15:29 -0700

    Add storyboard.

--- STItemPicker/AppDelegate.h
+++ STItemPicker/AppDelegate.h
@@ -1,10 +1,4 @@
-//
-//  AppDelegate.h
-//  STItemPicker
-//
-//  Created by Simon Toens on 3/24/13.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
-//
+// @author Simon Toens 02/14/13
 
 #import <UIKit/UIKit.h>
 
