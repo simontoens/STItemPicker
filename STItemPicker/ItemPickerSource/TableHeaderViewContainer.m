27d89120113e64e02ebf5906bd5404c2b236ec2a
Simon Toens <simontoens@gmail.com>
2013-03-31 21:32:15 -0700

    Add image reflection effect.

--- STItemPicker/ItemPickerSource/TableHeaderViewContainer.m
+++ STItemPicker/ItemPickerSource/TableHeaderViewContainer.m
@@ -1,11 +1,13 @@
 // @author Simon Toens 03/30/13
 
 #import "TableHeaderViewContainer.h"
+#import "UIImageView+Reflection.h"
 
 @interface TableHeaderViewContainer()
 
 @property (nonatomic, weak) IBOutlet UIView *tableHeaderView;
 @property (nonatomic, weak) IBOutlet UIImageView *imageView;
+@property (nonatomic, weak) IBOutlet UIImageView *reflectedImageView;
 @property (nonatomic, weak) IBOutlet UILabel *label1;
 @property (nonatomic, weak) IBOutlet UILabel *label2;
 @property (nonatomic, weak) IBOutlet UILabel *label3;
@@ -15,7 +17,7 @@
 @implementation TableHeaderViewContainer
 
 @synthesize label1, label2, label3;
-@synthesize imageView;
+@synthesize imageView, reflectedImageView;
 @synthesize tableHeaderView;
 
 + (UIView *)newTableHeaderView:(UIImage *)image 
@@ -29,6 +31,7 @@
     [container.label1 setText:l1];
     [container.label2 setText:l2];
     [container.label3 setText:l3];
+    [container.reflectedImageView setImage:[container.imageView reflectedImageWithHeight:container.reflectedImageView.bounds.size.height]];
     return container.tableHeaderView;
 }
 
